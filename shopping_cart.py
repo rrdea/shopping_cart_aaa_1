331a8f86a44d0126806272888f5bcacec9b01e5e
Ebbryl Nur Rochman <ebbryl.rochman@nest.tokopedia.com>
2019-07-18 11:11:04 +0700

    percobaan 1

---
+++ shopping_cart.py
@@ -0,0 +1,22 @@
+class Listing:
+    def __init__(self, name, location, price, stock):
+        self.name = name
+        self.location = location
+        self.price = price
+        self.stock = stock
+    def shipping_cost(self):
+        return 0
+    def headline(self):
+        return "This is Headline"
+    def update_stock(self):
+        return self.stock + 1
+    
+class Shoppingcart:
+    def __init__(self, items, checkout_time, total_price):
+        self.items = items
+        self.checkout_time = checkout_time
+        self.total_price = total_price
+    def add_items(self):
+        return self.items + 1
+    def remove_items(self):
+        return self.items - 1
